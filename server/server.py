187b11f7c15730601de27775345b42f5e1efb238
Sasha <amosin2002@mail.ru>
2023-10-23 19:06:00 +0300

    Fixes

--- server/server.py
+++ server/server.py
@@ -3,29 +3,43 @@ import random
 import numpy as np
 
 
-def throw_dice():
-    result = []
-    for i in range(5):
-        random_number = random.randint(1, 6)
-        result.append(random_number)
-    return result
+class Player:
+    def __init__(self, name):
+        self.points = 0
+        self.name = name
+        self.numbers = [0 for _ in range(5)]
+
+    def throw_dices(self):
+        for i in range(5):
+            random_number = random.randint(1, 6)
+            self.numbers[i] = random_number
+        return self.numbers
+
+    def throw_again(self, indexes):
+        for index in indexes:
+            random_number = random.randint(1, 6)
+            self.numbers[index] = random_number
+        return self.numbers
+
+    def get_result_round_one(self):
+        best_points = -31
+        for i in range(1, 7):
+            count = 0
+            for num in self.numbers:
+                if num == i:
+                    count += 1
+            if count > 0:
+                best_points = max(best_points, (count - 3) * i)
+        self.points = best_points
+        return best_points
 
 
-def throw_again(numbers: np.array, indexes: np.array):
-    for index in indexes:
-        random_number = random.randint(1, 6)
-        numbers[index] = random_number
+class Play:
+    def __init__(self, players_names):
+        self.players = [Player(name) for name in players_names]
 
+    def get_results(self):
+        answer = []
+        for player in self.players:
+            answer.append(player.points)
 
-def get_result(numbers: np.array):
-    best_num = 0
-    max_count = 0
-    for i in range(1, 7):
-        count = 0
-        for num in numbers:
-            if num == i:
-                count += 1
-        if count >= max_count:
-            best_num = i
-            max_count = count
-    return (max_count - 3) * best_num
